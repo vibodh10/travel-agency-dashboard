83e180a7228e33c1e939cadba71d27b01db657f5
vibodh10 <ghimab@gmail.com>
2025-05-12 06:50:07 +0100

    Implement Routing, Dashboard Layout & UI, Auth Functionality

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Header} from "../../../components";
 
 const AllUsers = () => {
     return (
-        <div>Users Table</div>
+        <main className="dashboard wrapper">
+            <Header
+                title="Trips Page"
+                description="Check out our current users in real time"
+            />
+
+            All Users Page Contents
+        </main>
     )
 }
 export default AllUsers
